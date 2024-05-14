98ec2a94fc09e07f4cdb333836ffa263fa71cf30
Francesco Chemello <francesco.chemello.1@studenti.unipd.it>
2024-05-14 14:43:20 +0200

    [Working in progress!]: add the draft of the reader of datas

--- code/project.py
+++ code/project.py
@@ -5,52 +5,48 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-# processes an entire dataset of DNA strings to onehot
-# # -code from VinaMiner-
-# def DNA_to_onehot_dataset(dataset):
-#   options_onehot = {'A': [1,0,0,0,0],'C' :[0,1,0,0,0], 'G':[0,0,1,0,0] ,'T':[0,0,0,1,0],'N':[0,0,0,0,1]}
-#   onehot_data = []
-#   for row in dataset:
-#     onehot_data.append(map(lambda e: options_onehot[e], row))
-#   onehot_data = np.array(onehot_data)
-#   print(np.shape(onehot_data))
-#   return onehot_data 
+from tqdm import tqdm  # For a nice progress bar
 
-# # processes a DNA string to onehot
-# # -code from VinaMiner-
-# def DNA_to_onehot(dna_line):
-#   options_onehot = {'A': [1,0,0,0,0],'C' :[0,1,0,0,0], 'G':[0,0,1,0,0] ,'T':[0,0,0,1,0],'N':[0,0,0,0,1]}
-#   onehot_data = map(lambda e: options_onehot[e], dna_line)
-#   onehot_data = np.array(onehot_data)
-#   return onehot_data 
+#!- FUNCTIONS
+
+def DNA_to_onehot_dataset(dataset):
+  options_onehot = {'A': [1,0,0,0,0],'C' :[0,1,0,0,0], 'G':[0,0,1,0,0] ,'T':[0,0,0,1,0],'N':[0,0,0,0,1]}
+  onehot_data = []
+  for row in dataset:
+    onehot_data.append(map(lambda e: options_onehot[e], row))
+  onehot_data = np.array(onehot_data)
+  return onehot_data 
+
+# processes a DNA string to onehot
+def DNA_to_onehot(dna_line):
+  options_onehot = {'A': [1,0,0,0,0],'C' :[0,1,0,0,0], 'G':[0,0,1,0,0] ,'T':[0,0,0,1,0],'N':[0,0,0,0,1]}
+  onehot_data = map(lambda e: options_onehot[e], dna_line)
+  onehot_data = np.array(onehot_data)
+  return onehot_data
 
 
 #!- MAIN
+
+# Set the device to be used (GPU or CPU)
+device = th.device("cuda" if th.cuda.is_available() else "cpu")
+print("Device: ", device)
 
 # Read the input from the cvc file
 absolute_path = os.path.dirname(__file__)
 rel_path_train = 'data\\fullset_train.csv'
 rel_path_val = 'data\\fullset_validation.csv'
 rel_path_test = 'data\\fullset_test.csv'
-print("I read the data")
-# Read the input from the csv file
-# DNA string
-train_csv = pd.read_csv(os.path.join(absolute_path, rel_path_train), sep=",")
-val_csv = pd.read_csv(os.path.join(absolute_path, rel_path_val), sep=",")
-test_csv = pd.read_csv(os.path.join(absolute_path, rel_path_test), sep=",")
 
+# Training Set
+
+# Read the input from the csv file
+train_csv = pd.read_csv(os.path.join(absolute_path, rel_path_train), sep=",")
 # Drop the NaN values
 train_csv = train_csv.dropna()
-val_csv = val_csv.dropna()
-test_csv = test_csv.dropna()
-
 # Describe the data
 train_csv.describe()
-val_csv.describe()
-test_csv.describe()
 
-# Training
-
+# Get the data from the csv file
 train_data = train_csv.values
 # m = number of input samples
 m = train_data.shape[0]
@@ -58,8 +54,20 @@ print("Amount of data:",m)
 X_train = train_data[:m,1]
 Y_train = train_data[:m,2]
 
-X_train = np.array(X_train)
-Y_train = np.array(Y_train)
+# OneHot encoding for the training data
+X_train = DNA_to_onehot_dataset(X_train)
+
+print(type(X_train))
+print(type(X_train[0]))
+
+# The onehot encoding produce a map but we cannot use it as a tensor. We need to change the function in order
+# to return a list of lists and not a map of lists. 
+exit() # Debug -END OF ONEHOT ENCODING-
+
+
+
+X_train = th.from_numpy(np.array(X_train))
+Y_train = th.from_numpy(np.array(Y_train))
 
 print("X_train shape: ", X_train.shape)
 print("Y_train shape: ", Y_train.shape)
@@ -67,7 +75,17 @@ print("Y_train shape: ", Y_train.shape)
 print("X_train values: ", X_train)
 print("Y_train values: ", Y_train)
 
-# Validation
+# Free memory
+del train_csv, train_data, m
+
+# Validation Set
+
+# Read the input from the csv file
+val_csv = pd.read_csv(os.path.join(absolute_path, rel_path_val), sep=",")
+# Drop the NaN values
+val_csv = val_csv.dropna()
+# Describe the data
+val_csv.describe()
 
 val_data = val_csv.values
 # m = number of input samples
@@ -76,8 +94,11 @@ print("Amount of data:",m)
 X_val = val_data[:m,1]
 Y_val = val_data[:m,2]
 
-X_val = np.array(X_val)
-Y_val = np.array(Y_val)
+# OneHot encoding for the validation data
+X_val = DNA_to_onehot_dataset(X_val)
+
+X_val = th.from_numpy(np.array(X_val))
+Y_val = th.from_numpy(np.array(Y_val))
 
 print("X_val shape", X_val.shape)
 print("Y_val shape", Y_val.shape)
@@ -85,7 +106,17 @@ print("Y_val shape", Y_val.shape)
 print("X_val values: ", X_val)
 print("Y_val values: ", Y_val)
 
+# Free memory
+del val_csv, val_data, m
+
 # Test
+
+# Read the input from the csv file
+test_csv = pd.read_csv(os.path.join(absolute_path, rel_path_test), sep=",")
+# Drop the NaN values
+test_csv = test_csv.dropna()
+# Describe the data
+test_csv.describe()
 
 test_data = test_csv.values
 # m = number of input samples
@@ -94,14 +125,20 @@ print("Amount of data:",m)
 X_test = test_data[:m,1]
 Y_test = test_data[:m,2]
 
-X_test = np.array(X_test)
-Y_test = np.array(Y_test)
+# OneHot encoding for the test data
+X_test = DNA_to_onehot_dataset(X_test)
+
+X_test = th.from_numpy(np.array(X_test))
+Y_test = th.from_numpy(np.array(Y_test))
 
 print("X_test shape", X_test.shape)
 print("Y_test shape", Y_test.shape)
 
 print("X_test values: ", X_test)
 print("Y_test values: ", Y_test)
+
+# Free memory
+del test_csv, test_data, m
 
 exit() # Debug -END OF READING DATA-
 
@@ -110,11 +147,20 @@ exit() # Debug -END OF READING DATA-
 # val_onehot = th.from_numpy(val_onehot).float()
 # test_onehot = th.from_numpy(test_onehot).float()
 
+print("Start training the model")
+
 # RNN
 
-input_size = len(train_data)
+# Hyperparameters
+input_size = len(X_train)
 hidden_size = 64  # To be defined
-output_size = 1  # We want a probabilistic value
+output_size = 2  # We want a probabilistic value
+num_layers = 1
+num_classes = 2
+sequence_length = 1
+learning_rate = 0.005
+batch_size = 8
+num_epochs = 10
 
 # 1. Define the model
 # Parameters:
@@ -124,24 +170,24 @@ output_size = 1  # We want a probabilistic value
 #   bias – If False, then the layer does not use bias weights b_ih and b_hh. Default: True
 #   batch_first – If True, then the input and output tensors are provided as (batch, seq, feature) instead of (seq, batch, feature). Note that this does not apply to hidden or cell states. See the Inputs/Outputs sections below for details. Default: False
 #   dropout – If non-zero, introduces a Dropout layer on the outputs of each GRU layer except the last layer, with dropout probability equal to dropout. Default: 0
-#   bidirectional – If True, becomes a bidirectional GRU. Default: False
-GRU_model = th.nn.GRU(input_size, hidden_size, num_layers=2, bias=True, batch_first=False, dropout=0.0, bidirectional=True, device=None, dtype=None)
+#   bidirectional – If True, becomes a bidirectional GRU. Default: 
+model = th.nn.GRU(input_size, hidden_size, num_layers, num_classes, num_layers=2, bias=True, batch_first=False, dropout=0.0, bidirectional=True, device=None, dtype=None).to(device)
 # 2. Train the model with epochs
-criterion = nn.MSELoss()
-optimizer = th.optim.Adam(GRU_model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss()   #the most common loss function used for classification problems
+optimizer = th.optim.Adam(model.parameters(), lr=learning_rate)
 # 3. Test the model using epochs
 
-for epoch in range(10):
-    for inputs in train_data:  # Carica i dati di addestramento
-        # Print the first item of train_data to see its structure
-        print(next(iter(train_data)))
-        optimizer.zero_grad()
-        outputs, _ = GRU_model(inputs)
-        loss = criterion(outputs[:, -1, :], targets)
-        loss.backward()
-        optimizer.step()
+for epoch in range(num_epochs):
+    # TODO: Implement the training loop
+    break
+
+model.train()
+
+print("End of training the model")
+
+print("Start testing the model")
 
 # 4. Evaluate the model
-predicted_values, _ = GRU_model(test_data)
+predicted_values, _ = model(test_data)
 
-
+print("End of testing the model")
